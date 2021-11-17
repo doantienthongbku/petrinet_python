098ac4c6e41e4871a25628c565deafc6e106ac1d
Doan Tien Thong <doantienthongbku@gmail.com>
2021-11-17 19:55:34 +0700

    Optimize using

--- run.py
+++ run.py
@@ -1,32 +1,39 @@
 from random import choice
-import petri_net
+from petri_net import Place, PetriNet, Transition, In, Out
 
-# create list of token
+# initialize list of token
 M0 = [1, 2, 3, 2]
-ps = [petri_net.Place(m) for m in M0]
+ps = [Place(m) for m in M0]
 
 # create graph of PetriNet
 """
-Sample:
----------------<---------------
-|                             |
-|--> P0 --> T1 --> P1 --> T2--|
-            \           /  \
-             \         /    \---> P3
-              P2 -----/
+Example:
+---------------<--------------------
+|                                  |
+|--> P0 --> *T1* --> P1 --> *T2* --|
+             |              /  \
+             |             /    \---> P3
+             P2 ----------/
 """
 ts = dict(
-    t1=petri_net.Transition(
-        [petri_net.In(ps[0])],
-        [petri_net.Out(ps[1]), petri_net.Out(ps[2])]
+    t1=Transition(
+        [In(ps[0])],
+        [Out(ps[1]), Out(ps[2])]
     ),
-    t2=petri_net.Transition(
-        [petri_net.In(ps[1]), petri_net.In(ps[2])],
-        [petri_net.Out(ps[3]), petri_net.Out(ps[0])]
+    t2=Transition(
+        [In(ps[1]), In(ps[2])],
+        [Out(ps[3]), Out(ps[0])]
     )
 )
 
-firing_sequence = [choice(list(ts.keys())) for _ in range(10)]
+print(ts.keys())
 
-network = petri_net.PetriNet(ts)
+num_fires = 3
+# random fire in dict of transitions
+firing_sequence = [choice(list(ts.keys())) for _ in range(num_fires)]
+
+# Or you can hand-choice transition will be fire (length = num_fires)
+# firing_sequence = ['t1', 't2', 't1']
+
+network = PetriNet(ts)
 network.run(firing_sequence, ps)
